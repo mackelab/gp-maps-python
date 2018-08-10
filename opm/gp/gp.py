Fix bug (forgot premulitply by noise variance)

opm/gp/gp.py
@@ -3,7 +3,7 @@ from .helpers import get_2d_indices
 from .match_radial_component import match_radial_component
 from .prior import LowRankPrior
 from .noise import FixedNoise, LowRankNoise
-from .lowrank import premult_by_postcov
+from .lowrank import calc_postmean
 
 from ..opm import calculate_map
 
@@ -123,8 +123,7 @@ class GaussianProcessOPM():
         #    vr += np.kron(v, r)[:,np.newaxis]
 
         # TODO: this can be made more efficient by leveraging the low-rank stuff
-
-        self.mu_post = premult_by_postcov(mhat, N, prior=self.prior, noise=self.noise).T
+        self.mu_post = calc_postmean(mhat, N, prior=self.prior, noise=self.noise).T
         self.mu_post = self.mu_post.reshape((d, nx, ny))
 
         return self.mu_post, self.K_post

opm/gp/lowrank.py
@@ -67,7 +67,7 @@ def lowrank_leftdiv_double(x, D, G1, invR1, H1, G2, invR2, H2):
     y = H2.T @ y
     innerblock = invR2 + (H2.T @ lowrank_leftdiv(G2, D, G1, invR1, H1))
     y = np.linalg.solve(innerblock, y)
-    y = G2 @ y;
+    y = G2 @ y
     y = x - y
     y = lowrank_leftdiv(y, D, G1, invR1, H1)
 
@@ -127,3 +127,9 @@ def premult_by_postcov(x, N, prior, noise):
 
     y = lowrank_leftmult(y, prior.D, prior.G, R=None, H=None)
     return y
+
+
+def calc_postmean(mhat, N, prior, noise):
+    
+    mhat2 = lowrank_leftdiv(mhat, noise.D, noise.G)
+    return premult_by_postcov(mhat2, N, prior, noise)

opm/gp/noise.py
@@ -3,10 +3,20 @@ from sklearn.decomposition import FactorAnalysis
 
 
 class NoiseModel:
+    """ Base class for noise models, defining noise covariance and variance based on the low-rank structure.
+    """
     
     @property
     def covariance(self):
+        """ Covariance matrix: D + GG'
+        """
         return self.D + self.G @ self.G.T
+    
+    @property
+    def variance(self):
+        """ Variance (diagonal of covariance matrix)
+        """
+        return np.diag(self.covariance)
 
 
 class FixedNoise(NoiseModel):
@@ -27,6 +37,12 @@ class FixedNoise(NoiseModel):
 class LowRankNoise(NoiseModel):
 
     def __init__(self, method, q):
+        """ Initialize method and dimensionality
+        
+        Args:
+            method: noise fitting method (can be either 'factoran' or 'indep')
+            q: rank of GG', only needed for factor analysis model
+        """
         super().__init__()
         self.method = method
         self.q = q
@@ -37,15 +53,15 @@ class LowRankNoise(NoiseModel):
     def fit(self, V, R, mu=None):
         """ Fit the noise model given the posterior mean
 
-                Args:
-                    V: stimuli, N_cond x N_rep x d array, stimulus conditions for each trial
-                    R: responses, N_cond x N_rep x n_x x n_y array, responses from an experiment
-                    mu: posterior mean
-                    **noise_kwargs: contains 'method' and 'q'
+        Args:
+            V: stimuli, N_cond x N_rep x d array, stimulus conditions for each trial
+            R: responses, N_cond x N_rep x n_x x n_y array, responses from an experiment
+            mu: posterior mean
+            **noise_kwargs: contains 'method' and 'q'
 
-                Returns:
-                    sigma, n x n noise covariance matrix
-                """
+        Returns:
+            sigma, n x n noise covariance matrix
+        """
 
         d = V.shape[2]
         N = R.shape[0] * R.shape[1]