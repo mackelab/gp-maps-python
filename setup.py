a228eaff024e0749b92c459281f1137c1d77cdfd
Dominik Straub <dominikstraub@yahoo.com>
2020-03-04 12:01:08 +0100

    Version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gp-maps-python",
-    version="0.0.1",
+    version="0.0.2",
     author="Dominik Straub",
     author_email="straub@psychologie.tu-darmstadt.de",
     description="Gaussian Processes for orientation preference maps",
